2af9e0342a389f0df660e30706793fd76147c93d
nancy <smithnancy@outlook.com>
2022-10-27 00:42:41 +1100

    Delete added

--- client/src/components/ActivitiesDetails.js
+++ client/src/components/ActivitiesDetails.js
@@ -1,9 +1,24 @@
+import { useActivitiesContext } from '../hooks/useActivitiesContext'
 const ActivitiesDetails = ({ activity }) =>{
+    //dispatch to update the status
+    const { dispatch } = useActivitiesContext()
+
+    const handleClick = async () => {
+        const response = await fetch('/api/activities/'+activity._id,{method: 'DELETE'})
+        //the document to be deleted
+        const json = await response.json()
+        // check the response
+        if(response.ok){
+            dispatch({ type: 'DELETE_ACTIVITY', payload: json})
+        }
+    }
+
     return (
         <div className="activity-details">
             <h3>{activity.title}</h3>
             <p>{activity.comment}</p>
             <p>{activity.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
