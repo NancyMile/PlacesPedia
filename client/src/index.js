be538c771b94de19b7da0f4162aed823d1cee872
nancy <smithnancy@outlook.com>
2022-10-27 00:03:45 +1100

    react context added to index js

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+//import context component
+import { ActivitiesContextProvider } from './context/ActivityContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ActivitiesContextProvider>
+      <App />
+    </ActivitiesContextProvider>
   </React.StrictMode>
 );
