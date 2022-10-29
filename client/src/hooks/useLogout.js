bfb3f6a89d444df566622d364cda041f8019ef55
NancyMile <smithnancy@outlook.com>
2022-10-29 21:37:36 +1100

    client, cleaning the activities  from local once the user logs out

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,7 +1,11 @@
 import { useAuthContext } from "./useAuthContext"
+// import activities context
+import { useActivitiesContext } from './useActivitiesContext'
 export const useLogout = () =>{
     //use the Auth hook
     const { dispatch } = useAuthContext()
+    //use dispatch from activities
+    const { dispatch: activitiesDispatch } = useActivitiesContext()
         
     //function logout 
     const logout = () =>{
@@ -9,6 +13,8 @@ export const useLogout = () =>{
         localStorage.removeItem('user')
         //logout action
         dispatch({type:'LOGOUT'})
+        //clear activities from local when logout
+        activitiesDispatch({type: 'SET_ACTIVITIES', payload: null})
     }
     return {logout}
 }
