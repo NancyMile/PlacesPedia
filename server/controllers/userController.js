4a26ff61496a2e206e2ab4bf3fe586fc8aead5a7
NancyMile <smithnancy@outlook.com>
2022-10-29 12:29:05 +1100

    jsonwebtoken package addedn and token created for signup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,11 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+const { create } = require('../models/userModel')
+//create token
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
+
 //login
 const loginUser = async (req, res) => {
 //set back the response
@@ -9,7 +16,9 @@ const signupUser = async (req, res) => {
      const {email, password } = req.body
      try {
          const user = await User.signup(email, password)
-         res.status(200).json({email, user})
+         //create token
+         const token = createToken(user._id)
+         res.status(200).json({email, token})
 
      } catch (error){
          res.status(400).json({error: error.message})
