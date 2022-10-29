de4b1bcc9a757294dabadd940e9fa61a16dfd3e8
NancyMile <smithnancy@outlook.com>
2022-10-29 17:55:10 +1100

    client, login hook created

---
+++ client/src/hooks/useLogin.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useAuthContext } from './useAuthContext'
+export const useLogin = () => {
+    //states
+    const[error, setError] = useState(null)
+    //loading state or dissable state on button
+    const[isloading, SetIsLoading] = useState(null)
+    const{ dispatch } = useAuthContext()
+
+     const login = async (email, password) => {
+        //every time sign up request reset
+        SetIsLoading(true)
+        setError(null)
+        // make post request
+        const response = await fetch ('/api/user/login',{ method:'POST', headers: {'Content-Type':'application/json'}, body: JSON.stringify({email, password})})
+        //response
+        const json = await response.json()
+        //check the response is is no ok breturnthe error
+        if (!response.ok){
+            SetIsLoading(false)
+            setError(json.error)
+        }
+        if(response.ok){
+            //save token on local storage
+            localStorage.setItem('user', JSON.stringify(json))
+            //update context
+            dispatch({type:'LOGIN', payload: json})
+            //set loading to false
+            SetIsLoading(false)
+        }
+    }
+    return {login, isloading, error }
+}
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
+    //hook parameters
+    const {login, isloading, error } = useLogin()
+
     //handle submit function
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(email, password)
+        await login(email, password)
     }
     // return template
      return (
@@ -16,7 +20,8 @@ const Login = () => {
             <input type="email" onChange={(e) => setEmail(e.target.value)} value = {email} />
             <label>Password:</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value = {password} />
-            <button>Login</button>
+            <button disabled={isloading}>Login</button>
+            { error && <div className='error'>{error}</div>}
         </form>
      )
 }
