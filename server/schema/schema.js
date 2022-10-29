b59d12d7062910cb214f0744802f1e13e4760b16
NancyMile <smithnancy@outlook.com>
2022-10-30 10:14:21 +1100

    server, modules included on schema and resolve functions updated

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,11 @@
 const graphql = require('graphql')
 const_ = require('lodash') //find data or change data
+//get models
+const Activity = require('../models/activityModel');
+const User = require('../models/userModel');
+
+
+
 //get function GraphQlObjectType
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLList } = graphql
 //define a new type
@@ -12,7 +18,8 @@ const ActivityType = new GraphQLObjectType({
         user:{
             type: UserType,
             resolve(parent, args){
-                return _.find(users, {id:parent.user_id});
+                //return _.find(users, {id:parent.user_id});
+                return User.findById(parent.user_id)
             }
         }
     })
@@ -27,7 +34,8 @@ const UserType = new GraphQLObjectType({
         activities:{
             type: new GraphQLList(ActivityType),
             resolve(parent, args){
-                return _.filter(activities, {user_id: parent.id})
+               // return _.filter(activities, {user_id: parent.id})
+               return Activity.find({user_id:parent.id})
             }
         }
     })
@@ -44,30 +52,40 @@ const RootQuery = new GraphQLObjectType({
             args:{ id:{type:GraphQLID}},
             resolve(parent, args){
                 //get data from the db to the source
-               return  _.find(activities, {id: args.id});
+               //return  _.find(activities, {id: args.id});
+               return Activity.findById(args.id)
             }
         },
         user:{
             type: UserType,
             args:{id:{type:GraphQLID}},
             resolve(parent, args){
-                return _.find(users,{id: args.id})
+                //return _.find(users,{id: args.id})
+                return User.findById(args.id)
             }
         },
         activities:{
             type: new GraphQLList(ActivityType),
             resolve(parent, args){
-                return activities
+                //return activities
+                return Activity.find({})
             }
         },
         users:{
             type: new GraphQLList(UserType),
             resolve(parent, args){
-                return users
+               // return users
+               return User.find({})
             }
         }
     }
 });
+
+//mutations edit or change data
+// const mutation = new GraphQLObjectType({
+
+// })
+
 
 module.exports = new GraphQLSchema({
     query: RootQuery
