9768cffcdaa1c757000f9d7e5b3b897b3bff3d71
NancyMile <smithnancy@outlook.com>
2022-10-31 23:06:16 +1100

    index js path  edited

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from 'App';
+import App from './App';
 //import context component
 import { ActivitiesContextProvider } from './context/ActivityContext'
 // import auth context
